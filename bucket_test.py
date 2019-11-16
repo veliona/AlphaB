6ea757fb10fe82f07e9582af975b660d1506468c
veliona <jakubczak.urszula.julia@gmail.com>
2019-11-16 21:08:10 +0100

    Add groupby initialization logic

--- bucket_test.py
+++ bucket_test.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
 
-from enum import Enum
 import matplotlib.pyplot as plt
-
-
-class TestType(Enum):
-    ABTESTING = 1
-    ABCTESTING = 2
-    ABCDTESTING = 3
-    ABCDETESTING = 4
+import pandas as pd
 
 
 class BucketTest:
     """ BucketTest class computes and renders charts and statistics for bucket testing. """
 
-    def __init__(self, test_type: TestType, y_axis: str, category_name: str, filter_name: str, categories: dict, x_axis="date"):
+    def __init__(self, y_axis: str, category_name: str, filter_name: str, categories: dict, x_axis="date"):
         """ Create a new bucket test with the given attributes """
-        self.test_type = test_type
+        # TODO: This should take and set a pd.Dataframe.
         self.y_axis = y_axis
         self.x_axis = x_axis
         self.category_name = category_name
         self.filter_name = filter_name
         self.categories = categories
+        # TODO: Call all groupby functions with self.
 
     def render(self, figure_size_x=12, figure_size_y=5, line_width=3, title_font_size=16, legend_font_size=14):
         """ Render renders the charts representing the bucket test """
@@ -30,12 +24,14 @@ class BucketTest:
         self.__build_chart_axis__(line_width)
 
         plt.title('{self.y_axis} per {self.category_name} for {filter_name}', fontsize=title_font_size)
-        plt.legend(bbox_to_anchor=(1.3, 0.8), frameon=False, fontsize=legend_font_size)
+        plt.legend(bbox_to_anchor=(1.3, 0.8), frameon=False, fontsize=legend_font_size, title=self.category_name)
         plt.ylabel(self.y_axis)
 
         # TODO: Calculate ylim
         # plt.ylim(0, 7000)
 
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
         plt.xticks(rotation=30)
         plt.show()
 
@@ -45,17 +41,25 @@ class BucketTest:
         # TODO: Compute PValue between A-B, A-C, B-C, etc.
         print("not implemented")
 
-    def __build_chart_axis__(self, line_width):
+    def __build_chart_axis__(self, line_width, category):
         # TODO: Logic to put data into plot
-        print("not implemented")
-        # for category in self.categories:
-        #   for wiki in df_wiki['wiki'].unique():
-        #   df_plot_wiki = df_wiki[df_wiki['wiki'] == wiki]
-        #   df_plot_wiki.set_index('date', inplace=True)
-        #   ax.plot(df_plot_wiki['impressions'], label=wiki, linewidth=line_width)
+        # for category in self.categories.unique():
+        #    df_plot_category = df_category[df_category[category] == category]
+        #    df_plot_category.set_index('date', inplace=True)
+        #    ax.plot(df_plot_category[self.filter_name], label=category, linewidth=line_width)
 
     def __set_locator_and_formatter__(self):
         # TODO: Set major locator and formatter for x_axis
         print("not implemented")
         # ax.x_axis.set_major_locator(mdates.DayLocator(interval=15))
         # ax.x_axis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+
+def GroupBy(column_name: str, groups: dict):
+    """ Calls group by with the given arguments """
+    def groupBy(test: BucketTest):
+        # groupBy is in the scope of GroupBy so it has access to the column name and groups.
+        # TODO: Call groupBy on the dataframe with the given arguments
+        # TODO: Set the groups and grouped column names in the BucketTest as attributes.
+        # TODO: Return the grouped dataframe
+    return groupBy
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
-from bucket_test import TestType, BucketTest
+from bucket_test import BucketTest, GroupBy
 
 
 def main():
     bucket_test = BucketTest(
-        test_type=TestType.ABTESTING,
+        #dataframe=df,
         y_axis="CTR",
-        category_name="design",
+        # category_name="design",
         filter_name="device",
-        categories={
-            'A': ['10303394', '03948402', '30495043', '30495906'],
-            'B': ['04342343', '64748877', '33677675', '98657939'],
-        },
+        # categories={
+        #     'A': ['10303394', '03948402', '30495043', '30495906'],
+        #     'B': ['04342343', '64748877', '33677675', '98657939'],
+        # },
         x_axis="date",
+        GroupBy(column_name="design", groups={
+                'old': ['10303394', '03948402', '30495043', '30495906'],
+                'new': ['04342343', '64748877', '33677675', '98657939'],
+        }),
+        GroupBy(column_name="device", groups={
+                'desktop': ['desktop'],
+                'mobile': ['mobile'],
+        }),
     )
 
     bucket_test.render()
