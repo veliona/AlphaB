f160168e7b904fd4ac123628a4f3c37d8e56aae2
veliona <jakubczak.urszula.julia@gmail.com>
2019-11-17 18:56:37 +0100

    Basic chart initialization

--- bucket_test.py
+++ bucket_test.py
@@ -7,32 +7,37 @@ import pandas as pd
 class BucketTest:
     """ BucketTest class computes and renders charts and statistics for bucket testing. """
 
-    def __init__(self, y_axis: str, category_name: str, filter_name: str, categories: dict, x_axis="date"):
+    def __init__(self, df: pd.DataFrame, y_axis: str, groups: list, x_axis="date"):
         """ Create a new bucket test with the given attributes """
-        # TODO: This should take and set a pd.Dataframe.
+        self.df = df
         self.y_axis = y_axis
         self.x_axis = x_axis
-        self.category_name = category_name
-        self.filter_name = filter_name
-        self.categories = categories
-        # TODO: Call all groupby functions with self.
+        self.groups = groups
+        # TODO: Set time interval (each day, each week, etc.)
+        # TODO: Custom y_label value (ex. [%])
+
 
     def render(self, figure_size_x=12, figure_size_y=5, line_width=3, title_font_size=16, legend_font_size=14):
         """ Render renders the charts representing the bucket test """
-        plt.subplots(figsize=(figure_size_x, figure_size_y))
+        fig, ax = plt.subplots(figsize=(figure_size_x, figure_size_y))
 
-        self.__build_chart_axis__(line_width)
-
-        plt.title('{self.y_axis} per {self.category_name} for {filter_name}', fontsize=title_font_size)
-        plt.legend(bbox_to_anchor=(1.3, 0.8), frameon=False, fontsize=legend_font_size, title=self.category_name)
+        # TODO: Improve title to be `per groups[0] and group[1]...` and so on.
+        plt.title('{} per {}'.format(self.y_axis, self.groups), fontsize=title_font_size)
+        plt.legend(bbox_to_anchor=(1.3, 0.8), frameon=False, fontsize=legend_font_size)
         plt.ylabel(self.y_axis)
 
-        # TODO: Calculate ylim
-        # plt.ylim(0, 7000)
+        for group_one_value in self.df[self.group[0]].unique():
+            self.df = self.df[self.df[self.group[0]] == group_one_value]
+            if len(self.group) > 1:
+                for group_two_value in self.df[self.group[1]].unique():
+                    self.df = self.df[self.df[self.group[1]] == group_two_value]
+                    self.df.set_index(self.x_axis, inplace=True)
+                    ax.plot(self.df[self.y_axis], label=group_two_value, line_width=line_width)
 
-        ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.xticks(rotation=30)
+        # plt.ylabel('[%]')
+        plt.ylim(0, max(self.y_axis))
+        plt.xticks(rotation=30);
+        self.__set_locator_and_formatter__(ax)
         plt.show()
 
     def compute_pvalues(self):
@@ -41,25 +46,7 @@ class BucketTest:
         # TODO: Compute PValue between A-B, A-C, B-C, etc.
         print("not implemented")
 
-    def __build_chart_axis__(self, line_width, category):
-        # TODO: Logic to put data into plot
-        # for category in self.categories.unique():
-        #    df_plot_category = df_category[df_category[category] == category]
-        #    df_plot_category.set_index('date', inplace=True)
-        #    ax.plot(df_plot_category[self.filter_name], label=category, linewidth=line_width)
-
-    def __set_locator_and_formatter__(self):
+    def __set_locator_and_formatter__(self, ax):
         # TODO: Set major locator and formatter for x_axis
-        print("not implemented")
-        # ax.x_axis.set_major_locator(mdates.DayLocator(interval=15))
-        # ax.x_axis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-
-
-def GroupBy(column_name: str, groups: dict):
-    """ Calls group by with the given arguments """
-    def groupBy(test: BucketTest):
-        # groupBy is in the scope of GroupBy so it has access to the column name and groups.
-        # TODO: Call groupBy on the dataframe with the given arguments
-        # TODO: Set the groups and grouped column names in the BucketTest as attributes.
-        # TODO: Return the grouped dataframe
-    return groupBy
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
 
 from bucket_test import BucketTest, GroupBy
+import pandas as pd
 
 
 def main():
+    df = pd.DataFrame()
+
     bucket_test = BucketTest(
-        #dataframe=df,
-        y_axis="CTR",
-        # category_name="design",
-        filter_name="device",
-        # categories={
-        #     'A': ['10303394', '03948402', '30495043', '30495906'],
-        #     'B': ['04342343', '64748877', '33677675', '98657939'],
-        # },
-        x_axis="date",
-        GroupBy(column_name="design", groups={
-                'old': ['10303394', '03948402', '30495043', '30495906'],
-                'new': ['04342343', '64748877', '33677675', '98657939'],
-        }),
-        GroupBy(column_name="device", groups={
-                'desktop': ['desktop'],
-                'mobile': ['mobile'],
-        }),
+        df=df,
+        y_axis='impressions',
+        groups=['design', 'device'],
+        x_axis='date',
     )
 
     bucket_test.render()
