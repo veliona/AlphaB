f68cf99f2bdf90d685250be925704c9b0e32ffa0
veliona <jakubczak.urszula.julia@gmail.com>
2019-11-26 22:17:15 +0100

    Add __init__.py file, remove jupyter cache & update example

--- example/example.py
+++ example/example.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-from bucket_test import BucketTest, GroupBy
+from alphab import BucketTest
 import pandas as pd
 
 
 def main():
     df = pd.DataFrame()
     bucket_test = BucketTest(
-        df=df[df['device'] == 'mobile'],
+        df=df,
         y_axis='impressions',
         group='design',
         x_axis='date',
-        custom_title='Impressions by design for mobile',
+        custom_title='Impressions by design',
         custom_ylabel='#',
         custom_day_interval=1
     )
