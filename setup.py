3a8e234e0292144fffc5549cef8a425ab3218146
veliona <jakubczak.urszula.julia@gmail.com>
2019-11-26 21:44:09 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='alphab',
+     version='0.1',
+     author="Julia Jakubczak",
+     author_email="jakubczak.urszula.julia@gmail.com",
+     description="Python library for rendering charts and computing statistics for A/B testing",
+     long_description_content_type="text/markdown",
+     url="https://github.com/Wikia/AlphaB",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
